501332d03ba3da400317a8f1c6c579025db5fd26
zhangxiaodong1 <zhangxiaodong1@hisense.com>
2021-06-23 09:46:11 +0800

    myTest

--- LeanbackShowcase/app/src/main/java/androidx/leanback/leanbackshowcase/app/page/PageAndListRowFragment.java
+++ LeanbackShowcase/app/src/main/java/androidx/leanback/leanbackshowcase/app/page/PageAndListRowFragment.java
@@ -16,8 +16,17 @@ package androidx.leanback.leanbackshowcase.app.page;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
 import androidx.leanback.app.BackgroundManager;
 import androidx.leanback.app.BrowseFragment;
 import androidx.leanback.app.RowsFragment;
@@ -40,13 +49,7 @@ import androidx.leanback.widget.PresenterSelector;
 import androidx.leanback.widget.Row;
 import androidx.leanback.widget.RowPresenter;
 import androidx.leanback.widget.VerticalGridPresenter;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+
 import com.google.gson.Gson;
 
 /**
@@ -74,13 +77,17 @@ public class PageAndListRowFragment extends BrowseFragment {
         mBackgroundManager = BackgroundManager.getInstance(getActivity());
         mBackgroundManager.attach(getActivity().getWindow());
         getMainFragmentRegistry().registerFragment(PageRow.class,
-                new PageRowFragmentFactory(mBackgroundManager));
+                new PageRowFragmentFactory(mBackgroundManager, getActivity()));
     }
 
     private void setupUi() {
+        //header指的是侧边栏，是否默认展示
         setHeadersState(HEADERS_ENABLED);
+        //在内容区按返回键，是否返回到侧边栏
         setHeadersTransitionOnBackEnabled(true);
+        //侧边栏背景图
         setBrandColor(getResources().getColor(R.color.fastlane_background));
+//        setBrandColor(getResources().getColor(R.color.app_guidedstep_actions_background));
         setTitle("Title goes here");
         setOnSearchClickedListener(new View.OnClickListener() {
 
@@ -92,6 +99,11 @@ public class PageAndListRowFragment extends BrowseFragment {
             }
         });
 
+        /**
+         * TODO：转场效果是如何实现的，目前的效果是
+         * 1）侧边栏和内容区进入和退出时，有相应的动画；注释掉之后，进入的时候没有动画，但
+         * 2）2000的时候有loading，1000的时候没有loading？
+         */
         prepareEntranceTransition();
     }
 
@@ -126,11 +138,13 @@ public class PageAndListRowFragment extends BrowseFragment {
         mRowsAdapter.add(pageRow4);
     }
 
-    private static class PageRowFragmentFactory extends BrowseFragment.FragmentFactory {
+    private static class PageRowFragmentFactory extends FragmentFactory {
         private final BackgroundManager mBackgroundManager;
+        private final Context mContext;
 
-        PageRowFragmentFactory(BackgroundManager backgroundManager) {
+        PageRowFragmentFactory(BackgroundManager backgroundManager, Context context) {
             this.mBackgroundManager = backgroundManager;
+            this.mContext = context;
         }
 
         @Override
@@ -138,8 +152,11 @@ public class PageAndListRowFragment extends BrowseFragment {
             Row row = (Row)rowObj;
             mBackgroundManager.setDrawable(null);
             if (row.getHeaderItem().getId() == HEADER_ID_1) {
+                //此处，用来给每个fragment设置背景图
+                mBackgroundManager.setDrawable(mContext.getDrawable(R.drawable.background_food));
                 return new SampleFragmentA();
             } else if (row.getHeaderItem().getId() == HEADER_ID_2) {
+                mBackgroundManager.setDrawable(mContext.getDrawable(R.drawable.thumbnail_example_grid));
                 return new SampleFragmentB();
             } else if (row.getHeaderItem().getId() == HEADER_ID_3) {
                 return new SettingsFragment();
